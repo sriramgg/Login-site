c14937b08d43faa32a815cbc174cd145e2280ca4
sri <146156147+sriramgg@users.noreply.github.com>
2025-04-17 14:20:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function login() {
     alert("Enter email and password");
   }
 }
+
+function registerUser() {
+  const name = document.getElementById("regName").value;
+  const email = document.getElementById("regEmail").value;
+  const password = document.getElementById("regPassword").value;
+
+  if (name && email && password) {
+    const otp = Math.floor(100000 + Math.random() * 900000);
+    localStorage.setItem("userEmail", email);
+    localStorage.setItem("userOTP", otp);
+    alert("OTP Sent: " + otp); // For demo only
+    window.location.href = "verify.html";
+  } else {
+    alert("Fill all fields");
+  }
+}
